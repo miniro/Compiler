785e0ec04ee02780159cac7701123d977bce0179
ftt <848564778@qq.com>
2018-12-09 23:49:34 +0800

    add ++ -- += -= *= /= %= do-while in leading end

--- cminus/Absyn.fs
+++ cminus/Absyn.fs
@@ -1,39 +1,31 @@
 module Absyn
 
 type typ =
-  | TypInt
-  | TypDouble
-  | TypBool
-  | TypChar
-  | TypLongLong
-  | TypArray of typ * int option
-  | TypPoint of typ
+  | TypI                           
+  | TypC                           
+  | TypA of typ * int option      
+  | TypP of typ                    
 
 and access =                                                      
   | AccVar of string                 
   | AccDeref of expr                
-  | AccIndex of access * expr        
+  | AccIndex of access * expr
 
 and expr =                                                         
-  | Access of access                
-  | Assign of access * expr         
-  | Addr of access                  
-  | CstInt of int
-  | CstDouble of double
-  | CstBool of bool
-  | CstChar of char          
-  | Prim1 of string * expr          
-  | Prim2 of string * expr * expr
-  | Prim3 of string * expr * expr * expr 
+  | Access of access              
+  | Assign of access * expr        
+  | Addr of access                 
+  | CstI of int                      
+  | Prim1 of string * expr           
+  | Prim2 of string * expr * expr    
   | Andalso of expr * expr          
-  | Orelse of expr * expr          
-  | Call of string * expr list      
+  | Orelse of expr * expr           
+  | Call of string * expr list       
 
 and stmt =
   | If of expr * stmt * stmt
-  | Switch of expr * (expr * stmt) list * stmt
   | While of expr * stmt
-  | DoWhile of stmt * expr
+  | Do of stmt * expr
   | Expr of expr
   | Return of expr option
   | Block of stmtordec list
